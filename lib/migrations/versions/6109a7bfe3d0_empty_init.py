4600d9e17817624a2f5d53cf2cb04b93db6fb93c
teklumezgebo <mezge002@umn.edu>
2024-01-12 18:20:15 -0600

    complete

---
+++ lib/migrations/versions/6109a7bfe3d0_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 6109a7bfe3d0
+Revises: 
+Create Date: 2024-01-12 18:09:20.070361
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6109a7bfe3d0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
